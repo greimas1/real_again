89dd064a32fc8aa916f92e5dcbdd7c2cf0e945b1
greimas1 <occult183183@gmail.com>
2025-11-11 21:19:13 +0900

    Add files via upload

---
+++ 9.1.1_2025.11.11.R
@@ -0,0 +1,22 @@
+library(dplyr)
+rdata <- read.csv("loan.csv", fileEncoding="Euc-kr")
+
+str(rdata)
+colSums(is.na(rdata))
+
+data <- rdata %>% group_by(성별) %>% 
+  summarise(K은행대출합=sum(K은행대출), W은행대출합=sum(W은행대출))
+print(data)
+#남자        3864       124350
+#여자         535.       16500
+
+loan_man <- rdata %>% filter(성별=="남자") %>% arrange(desc(W은행대출))
+loan_woman <- rdata %>% filter(성별=="여자") %>% arrange(desc(W은행대출))
+
+loan_man$대출금차이 <- abs(loan_man$W은행대출 - loan_man$K은행대출)
+loan_woman$대출금차이 <- abs(loan_woman$W은행대출 - loan_woman$K은행대출)
+
+print(loan_man$지점코드[which.max(loan_man$대출금차이)])
+#190124
+print(loan_woman$지점코드[which.max(loan_woman$대출금차이)])
+#190114
